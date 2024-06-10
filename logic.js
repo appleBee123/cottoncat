63ee2e730adf7d91346910783afa0e4dc3017f93
appleBee123 <99538945+appleBee123@users.noreply.github.com>
2024-06-10 13:26:59 +0530

    div

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 
+
 fetch("https://meowfacts.herokuapp.com/")
   .then((response) => response.json())
   //   .then((json) => console.log(json));
@@ -7,7 +8,7 @@ fetch("https://meowfacts.herokuapp.com/")
 // above is a GET request to retrieve a cat fact using an API
 
 j = 0
-catArray = ["cat8.jpg", "cat2.jpg", "cat3.jpg", "cat4.webp", "cat5.webp", "cat6.webp", "cat7.jpg", "cat.webp"]   
+catArray = ["cat8.jpg", "cat2.jpg", "cat3.jpg", "cat4.webp", "cat5.webp", "cat6.webp", "cat7.jpg", "cat.webp"]
 // 8 images
 
 function catLoad() {
@@ -21,3 +22,41 @@ function display() {
     j = 0
   }
 }
+
+
+
+// code starts for image change in div 
+imgCounter = 0
+
+$(document).ready(function () {
+  $("#showdiv").mouseenter(function () {
+   
+
+    //Refresh the thumbnail
+    $(".photo-container").animate({
+      opacity: 0.10,
+    }, 200, function () {
+
+      if (imgCounter == 0) {
+        $(".photo-info").attr("src", "images/snowman.jpeg");
+        imgCounter++
+      }
+      else if (imgCounter == 1) {
+        $(".photo-info").attr("src", "images/tree.jpeg");
+        imgCounter++
+      }
+      else {
+        $(".photo-info").attr("src", "images/gift.webp");
+        imgCounter=0
+      }
+
+      
+      //ReloadGallery(currentContext); // Just showing extra logic can be use here...
+
+    }).animate({ opacity: 1 }, 200)
+
+  })
+})
+// code ends for image change in div 
+
+
